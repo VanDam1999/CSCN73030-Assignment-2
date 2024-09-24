6e8738ac32a632d92a7ee62c7a5b622929d1962e
VanDam1999 <ianvandam99@gmail.com>
2024-09-24 19:39:15 -0400

    Created pre-release directive and added pre-release source code

--- Assignment 2/Source.cpp
+++ Assignment 2/Source.cpp
@@ -6,38 +6,68 @@
 #include <vector>
 using namespace std;
 
+#define PRE_RELEASE
+
 struct STUDENT_DATA {
 	std::string firstname;
 	std::string lastname;
+	std::string email;
 };
 
 std::vector<STUDENT_DATA> students;
 
-int main() {
-	std::ifstream file("StudentData.txt");
-	std::string str{};
+int main() { 
+
+	// PRE-RELEASE MODE SORUCE CODE
+	#ifdef PRE_RELEASE
+		cout << "You are currently running the pre-release source code \n" << endl;
+		
+		// Open the StudentData_Email.txt file
+		std::ifstream file("StudentData_Emails.txt");
+	#else 
+		cout << "You are currently running the standard source code \n" << endl;
+
+		// Open the StudentData.txt file
+		std::ifstream file("StudentData.txt");
+	#endif;
+
+	std::string str;
+	
+	// Read each line from the txt file.
 	while (std::getline(file, str)) {
 		
-		std::istringstream iss(str);
+		std::istringstream new_str(str);
 
 		std::string new_string;
 		std::vector<std::string> new_strings;
 
-		while (std::getline(iss, new_string, ',')) {
+		// Break the string from the file into a vector where there is a comma
+		while (std::getline(new_str, new_string, ',')) {
 			new_strings.push_back(new_string);
 		}
 
+		// Create an object and store the vector data into it
 		STUDENT_DATA student;
 		student.firstname = new_strings[0];
 		student.lastname = new_strings[1];
+		student.email = new_strings[2];
+
+		// Push the object into a vector object of the same type
 		students.push_back(student);
 	}
 
 	// DEBUG MODE TESTING
 	#ifdef _DEBUG
-		for (int i = 0; i < students.size(); i++) {
-			cout << students.at(i).firstname << "," << students.at(i).lastname << endl;
-		}
-	#endif
+		#ifdef PRE_RELEASE
+			for (int i = 0; i < students.size(); i++) {
+				cout << students.at(i).firstname << "," << students.at(i).lastname << ", " << students.at(i).email << endl;
+			}
+		#else 
+			for (int i = 0; i < students.size(); i++) {
+				cout << students.at(i).firstname << "," << students.at(i).lastname << endl;
+			}
+		#endif;
+	#endif 
+
 	return 1;
 }
