7777c1179f960919c911bbf8a36d6f30b16a8a67
VanDam1999 <ianvandam99@gmail.com>
2024-09-24 00:49:10 -0400

    Updated Source.cpp to store file content into vector

--- Assignment 2/Source.cpp
+++ Assignment 2/Source.cpp
@@ -1,3 +1,39 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <vector>
+using namespace std;
+
+struct STUDENT_DATA {
+	std::string firstname;
+	std::string lastname;
+};
+
+std::vector<STUDENT_DATA> students;
+
 int main() {
+	std::ifstream file("StudentData.txt");
+	std::string str{};
+	while (std::getline(file, str)) {
+		
+		std::istringstream iss(str);
+
+		std::string new_string;
+		std::vector<std::string> new_strings;
+
+		while (std::getline(iss, new_string, ',')) {
+			new_strings.push_back(new_string);
+		}
+
+		STUDENT_DATA student;
+		student.firstname = new_strings[0];
+		student.lastname = new_strings[1];
+		students.push_back(student);
+	}
+
+
+
+
 	return 1;
 }
